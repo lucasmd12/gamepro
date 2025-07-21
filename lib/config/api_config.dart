55252aef25e18dcdd6e9f34fe641ae729c5c39a6
lucasmd12 <lucasjuzahyder@gmail.com>
2025-07-21 20:32:50 -0300

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 // lib/config/api_config.dart
 
 class ApiConfig {
-  static const String baseUrl = "YOUR_API_BASE_URL_HERE"; // Replace with your actual API base URL
+  static const String baseUrl = "https://beckend-ydd1.onrender.com"; // Replace with your actual API base URL
   // Add other API configuration constants here if needed
 }
